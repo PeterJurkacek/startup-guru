57744da25157de7c19d0bbd114222d14bd011777
Peter Jurkacek <peter.jurkacek@aston.sk>
2024-03-06 22:30:51 +0100

    qrcodes

--- qrcodes.py
+++ qrcodes.py
@@ -4,6 +4,8 @@ links = [
     ("qr_aid.png", "https://www.aidental.ai"),
     # ("", "https://www.edu.aidental.ai"),
     ("qr_1.png", "https://www.linkedin.com/in/peter-jurkacek-a5966b209"),
+    ("qr_form_intro.png", "https://aidental.typeform.com/to/MLtakqKO"),
+    ("qr_form_feedback.png", "https://aidental.typeform.com/to/VKpVamog")
 ]
 
 for (name, url) in links:
